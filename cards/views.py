ecc62d5964533223ba158ceb9194c7c734acc2ee
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-11-28 19:19:36 -0300

    Limpando código

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from django.db.models import Q
 from . import models, serializers, paginators
 
 class List(generics.ListAPIView):
@@ -14,9 +13,7 @@ class List(generics.ListAPIView):
         
         if nome is not None:
             queryset = queryset.filter(
-                Q(
-                    Q(titulo__icontains = nome)
-                ),
+                titulo__icontains = nome,
                 mais_recente = True
             )
 
